61620ecdaa78544a0b9658e97186858f83113b6e
yuma <ikeoku.y@gmail.com>
2022-03-04 10:15:52 +0900

    最小値関数を可変長に対応させた

--- chapter3/minimum.js
+++ chapter3/minimum.js
@@ -1,1 +1,11 @@
 const min = (a, b) => a < b ? a : b
+
+{
+  // 可変長に対応
+  const min = (...nums) => {
+    let min = Infinity
+    nums.forEach(num => min = min > num ? num : min)
+    return min
+  }
+  console.log(min(4, -2, 3))
+}
--- chapter3/recursion.js
+++ chapter3/recursion.js
@@ -1,22 +1,25 @@
-// 負の数未対応
-const isEven = n => {
-  if (n === 0) {
-    return true
-  } else if( n === 1) {
-    return false
-  } else if ( n > 1) {
-    return isEven(n-2)
+{
+  // 負の数未対応
+  const isEven = n => {
+    if (n === 0) {
+      return true
+    } else if( n === 1) {
+      return false
+    } else if ( n > 1) {
+      return isEven(n-2)
+    }
+  }
+}
+// 負の数にも対応
+{
+  const isEven = n => {
+    if (n === 0) return true
+    else if (n === 1) return false
+    else if (n < 0) return isEven(-n)
+    else if (n > 1) return isEven(n)
   }
 }
 
-// 負の数にも対応
-const isEven2 = n => {
-  if (n === 0) return true
-  else if (n === 1) return false
-  else if (n < 0) return isEven(-n)
-  else if (n > 1) return isEven(n)
-}
-
 // 再帰ではなく2進数を使う
-const isEven3 = n => (n & 1) == 0
+const isEven = n => (n & 1) == 0
 
