36f3fb0edfdcc27970e553245be15c2a9580785b
yuma <ikeoku.y@gmail.com>
2022-05-26 10:47:18 +0900

    fizzbuzzのts版を解いた

---
+++ dist/fizzbuzz.js
@@ -0,0 +1,27 @@
+"use strict";
+const fizzbuzz = (n) => {
+    [...new Array(n).keys()].map((n) => n + 1).forEach((n) => {
+        if (n % 15 === 0)
+            console.log('FizzBuzz');
+        else if (n % 3 === 0)
+            console.log('Fizz');
+        else if (n % 5 === 0)
+            console.log('Buzz');
+        else
+            console.log(n);
+    });
+};
+// fizzbuzz(100)
+{ //別解
+    const fizzBuzz = (n) => {
+        [...new Array(n).keys()].map((n) => n + 1).forEach((n) => {
+            let result = '';
+            if (n % 3 === 0)
+                result += 'Fizz';
+            if (n % 5 === 0)
+                result += 'Buzz';
+            console.log(result || n);
+        });
+    };
+    fizzBuzz(100);
+}
